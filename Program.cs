01f30ac744f5c2649dac0a78c07d7d4d269ddfdc
Lucas Freire Salhab <freiresalhab10@gmail.com>
2025-08-05 18:03:38 -0300

    Primeiro commit - Exercício getUserName

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Program;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Olá " + nome +"!" + " Seja muito bem-vindo!" );
+    }
+}
